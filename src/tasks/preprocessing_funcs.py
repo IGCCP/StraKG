f3a09601f017058b6e0dc841e227ef0cb0d627ff
wli <39733492+IGCCP@users.noreply.github.com>
2023-08-05 10:49:54 -0700

    Update preprocessing_funcs.py

--- src/tasks/preprocessing_funcs.py
+++ src/tasks/preprocessing_funcs.py
@@ -3,12 +3,35 @@
 import pandas as pd
 from transformers import BertTokenizer
 
-def preprocess_data(data_path, tokenizer_path):
-    # Load data and tokenizer
-    data = pd.read_csv(data_path)
-    tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
+from transformers import BertTokenizer
+from tqdm import tqdm
+import torch
 
-    # Tokenize sentences
-    tokenized_data = data['sentence'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
-    return tokenized_data
+def preprocess_data(data, label_map, tokenizer, max_len):
+    sentences = data['sentence']
+    labels = data['label']
+    labels = [label_map[label] for label in labels]
+    tokens = []
+    token_ids = []
+    mask_ids = []
+    segment_ids = []
+    label_ids = []
 
+    for sentence in tqdm(sentences):
+        text_tokens = tokenizer.tokenize(sentence)
+        text_tokens = ["[CLS]"] + text_tokens + ["[SEP]"]
+        tokens.append(text_tokens)
+        token_id = tokenizer.convert_tokens_to_ids(text_tokens)
+        padding = [0] * (max_len - len(token_id))
+        mask_id = [1] * len(token_id) + padding
+        segment_id = [0] * max_len
+        token_id += padding
+        token_ids.append(token_id)
+        mask_ids.append(mask_id)
+        segment_ids.append(segment_id)
+
+    label_ids = labels
+    return torch.tensor(token_ids), torch.tensor(mask_ids), torch.tensor(segment_ids), torch.tensor(label_ids)
+
+label_map = {"named_by": 0, "year_named": 1, "isLocatedIn": 2, "hasThickness": 3, "contains": 4}
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
